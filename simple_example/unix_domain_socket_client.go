a39e3ba2761e03f9c5d0c30356bd8109c2a7f81d
Vinny Meller <vinnymeller@gmail.com>
2021-09-19 23:29:47 -0500

    first commit

---
+++ simple_example/unix_domain_socket_client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net"
+	"io"
+	"log"
+	"time"
+)
+
+func reader(r io.Reader) {
+	buf := make([]byte, 1024)
+	n, err := r.Read(buf[:])
+	if err != nil {
+		return
+	}
+	println("Client got:", string(buf[0:n]))
+}
+
+func main() {
+	c, err := net.Dial("unix", "/tmp/echo.sock")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+	
+	go reader(c)
+	_, err = c.Write([]byte("hi"))
+	if err != nil {
+		log.Fatal("write error:", err)
+	}
+
+	reader(c)
+	time.Sleep(100 * time.Millisecond)
+}
